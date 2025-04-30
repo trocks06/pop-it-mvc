bcb9f65fb03f4cda5f1cf71b62798249139731bb
gr422_hadal <denishatkov17@gmail.com>
2025-04-30 15:51:09 +0700

    add validators

--- views/site/login.php
+++ views/site/login.php
@@ -6,6 +6,7 @@
 if (!app()->auth::check()):
     ?>
     <form method="post">
+        <input name="csrf_token" type="hidden" value="<?= app()->auth::generateCSRF() ?>"/>
         <label>Логин <input type="text" name="login"></label>
         <label>Пароль <input type="password" name="password"></label>
         <button>Войти</button>
